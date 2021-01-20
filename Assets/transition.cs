73a8593290fcb323e1155c02baec85c00c6f7446
alla2001 <48186577+alla2001@users.noreply.github.com>
2021-01-20 23:07:39 +0100

    add files

---
+++ Assets/transition.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class transition : MonoBehaviour
+{
+    public bool left;
+    // Start is called before the first frame update
+    void Start()
+    {
+        RectTransform rt = this.GetComponent<RectTransform>();
+        rt.sizeDelta = new Vector2(Screen.width/2, rt.sizeDelta.y);
+        if (left)
+        {
+            rt.transform.localPosition = new Vector2((-Screen.width / 2 - Screen.width / 4), 0);
+        }
+        else
+        {
+            rt.transform.localPosition = new Vector2((Screen.width / 2 + Screen.width / 4), 0);
+        }
+       
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        
+    }
+}
